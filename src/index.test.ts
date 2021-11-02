54721ef65f2208be45279431a35888ebea434f03
Erin <erin20913@gmail.com>
2021-11-02 17:45:49 -0400

    Add basic functionality and tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,48 @@
 import test from 'ava';
-import {hello} from './index';
+import createDeprecationEmitter from './index';
 
-test('hello', t => {
-	t.is(hello('foo'), 'Hello, foo!');
+test('createDeprecationEmitter', t => {
+	// Mock out process.emitWarning with a function that just records call args
+	const emitWarningCallHistory: any[][] = [];
+	process.emitWarning = (...args) => {
+		emitWarningCallHistory.push(args);
+	};
+
+	// Create a deprecation emitter
+	const emitDeprecation = createDeprecationEmitter('foolib', {
+		FOO: 'some message',
+		BAR: 'a second message',
+	});
+
+	// Emit a single warning
+	emitDeprecation('FOO');
+	// process.emitWarning should have been called once with the correct args
+	t.is(emitWarningCallHistory.length, 1);
+	t.deepEqual(emitWarningCallHistory[0], [
+		'some message',
+		'DeprecationWarning',
+		'foolib:FOO',
+	]);
+
+	// Emit the same warning again
+	emitDeprecation('FOO');
+	// process.emitWarning should NOT have been called a second time
+	t.is(emitWarningCallHistory.length, 1);
+
+	// Emit a different warning
+	emitDeprecation('BAR');
+	// process.emitWarning should have been called a second time with args
+	t.is(emitWarningCallHistory.length, 2);
+	t.deepEqual(emitWarningCallHistory[1], [
+		'a second message',
+		'DeprecationWarning',
+		'foolib:BAR',
+	]);
+
+	// Emit a deprecation which doesn't exist; this should throw
+	t.throws(() => {
+		// explicit conversion to any because this is also against typings, but
+		// still want to test it for JS consumers
+		emitDeprecation('DOES_NOT_EXIST' as any);
+	});
 });
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,30 @@
 /**
- * Generates a greeting for a person.
- * @param name The name of the person
- * @returns A greeting for the person
+ * Creates a function for emitting deprecation warnings.
+ * @param libraryName A project name to ensure deprecation codes are unique
+ * @param warnings An object containing all warning codes and their messages
+ * @returns A function that can be called to emit deprecation warnings
  */
-export function hello (name) {
-	return `Hello, ${name}!`;
-}
+export default <T extends string>(libraryName: string, warnings: {[code in T]: string}) => {
+	// An array that keeps track of which warning codes have already been used
+	const emittedCodes: T[] = [];
+
+	/**
+	 * Emits a deprecation warning for the given code, unless a warning for that
+	 * code has already been emitted.
+	 * @param code The code of the warning to emit
+	 */
+	return (code: T) => {
+		// If this isn't a registered code, throw
+		if (!(code in warnings)) {
+			throw new Error(`"${code}" is not a deprecation`);
+		}
+		// If we've already emitted this code, don't emit it again
+		if (emittedCodes.includes(code)) {
+			return;
+		}
+		// Emit a DeprecationWarning with the given code and message
+		process.emitWarning(warnings[code], 'DeprecationWarning', `${libraryName}:${code}`);
+		// Record this code so we don't emit the warning a second time
+		emittedCodes.push(code);
+	};
+};
